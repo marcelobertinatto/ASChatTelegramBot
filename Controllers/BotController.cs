7eeb69d3895d362656f965f8e7bdac808673cd5a
marcelo <marcelobertinatto@hotmail.com>
2021-01-23 16:57:35 +0000

    initial bot

---
+++ Controllers/BotController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChatTelegramBot.Controllers
+{
+    [ApiController]
+    [Route("api/bot")]
+    public class BotController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            TelegramBotClient client = new TelegramBotClient("1417186445:AAGFG-jByzgAEhaZRAKLnnJOigAXbzM8dhU");
+
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+
+            return Ok();
+        }
+    }
+}
