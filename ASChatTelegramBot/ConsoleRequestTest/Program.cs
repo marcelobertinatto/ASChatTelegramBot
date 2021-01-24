56b87ac87e68ae04013e00cce65e41a7e838556a
Marcelo Bertinatto <marcelobertinatto@hotmail.com>
2021-01-24 22:29:39 +0000

    new project to test hotmart's api

---
+++ ASChatTelegramBot/ConsoleRequestTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ConsoleRequestTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string token = "H4sIAAAAAAAAAB2OyZKCMABEv8ipABLxiKCYCGHfcrEgoCxRURCErx9nDn3p5VWXM65yg9V2jf1wQQKpUY%2Funsw0BFHbJZGGtz%2FljJdU3E7URzCe8UzjQ4vqqS4k3BVGWCf%2BVNOkmlDz%2BJBm%2F9VVshr3Y2q4KhLvkf%2F3OGfz%2Fx4FgkfcCIeJQHZB62mBcP1nZHev%2BzJq65YuNLBksrDBCnBNfACo4cpm0C5ETAdi7Ndf%2F9tiotWQ6u9LKn46GssgEXn7xyBG%2BrEC9LEDNpCm4PYfI3YnM%2BCc6Opg6UyyAxdQ3WtI43F0Bz%2BR5kJP36NBSLO3DzvJj%2B%2BoviaV71ADHsNx6%2FTKwViy2yqFGNHTW1j3Yns4jv0tYmsdOS%2FOABhcYyW46vKS7VmhTy5Fql5eAM1H7flQUj2LHlny1EjfdYdai%2FB0Ia9xzYu88l97GbLPe50HQIW8bcgmrxPcRDvVN7SVeeKGxMTTORKdnXgC4GKzVR6%2BHfSNuy6WpbOjJnE5wlK5p7tjObGg6YdNqRSUnYdNeEMbmB2KwTPteVb2Qg%2FJHV4MzfSm7UjUpbyk9mdyGg%2Fx9niedaOrGc%2BCG5QLdH4uviZrvRgaEmmpuqOm4Asi1Z1K30KCb0oIkocDHrp0fYo4nAAXE%2Bs6qr%2BCWNTyYwIAAA%3D%3D";
+            var client = new RestClient("https://sandbox.hotmart.com/payments/api/v1/subscriptions?status=CANCELLED_BY_SELLER&status=ACTIVE");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", "Bearer " + token + "");
+            request.AddHeader("Content-Type", "application/json");            
+
+            var response = client.Execute(request);
+            var content = response.Content;
+            var signatures = JsonConvert.DeserializeObject<Signatures>(response.Content);
+            Console.WriteLine(content);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ASChatTelegramBot/ConsoleRequestTest/Signatures.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRequestTest
+{
+    public class Plan
+    {
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+    }
+
+    public class Product
+    {
+
+        [JsonProperty("id")]
+        public int id { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("ucode")]
+        public string ucode { get; set; }
+    }
+
+    public class Price
+    {
+
+        [JsonProperty("value")]
+        public double value { get; set; }
+
+        [JsonProperty("currency_code")]
+        public string currency_code { get; set; }
+    }
+
+    public class Subscriber
+    {
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("email")]
+        public string email { get; set; }
+
+        [JsonProperty("ucode")]
+        public string ucode { get; set; }
+    }
+
+    public class Item
+    {
+
+        [JsonProperty("subscriber_code")]
+        public string subscriber_code { get; set; }
+
+        [JsonProperty("subscription_id")]
+        public int subscription_id { get; set; }
+
+        [JsonProperty("status")]
+        public string status { get; set; }
+
+        [JsonProperty("accession_date")]
+        public long accession_date { get; set; }
+
+        [JsonProperty("request_date")]
+        public long request_date { get; set; }
+
+        [JsonProperty("trial")]
+        public bool trial { get; set; }
+
+        [JsonProperty("plan")]
+        public Plan plan { get; set; }
+
+        [JsonProperty("product")]
+        public Product product { get; set; }
+
+        [JsonProperty("price")]
+        public Price price { get; set; }
+
+        [JsonProperty("subscriber")]
+        public Subscriber subscriber { get; set; }
+    }
+
+    public class PageInfo
+    {
+
+        [JsonProperty("total_results")]
+        public int total_results { get; set; }
+
+        [JsonProperty("next_page_token")]
+        public string next_page_token { get; set; }
+
+        [JsonProperty("prev_page_token")]
+        public string prev_page_token { get; set; }
+
+        [JsonProperty("results_per_page")]
+        public int results_per_page { get; set; }
+    }
+
+    public class Signatures
+    {
+
+        [JsonProperty("items")]
+        public IList<Item> items { get; set; }
+
+        [JsonProperty("page_info")]
+        public PageInfo page_info { get; set; }
+    }
+
+
+}
