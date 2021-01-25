9ebb6c7651591801ea48817f0c1251f24b28f4ed
marcelo <marcelobertinatto@hotmail.com>
2021-01-25 13:44:27 +0000

    missing file

--- ASChatTelegramBot/ConsoleRequestTest/Program.cs
+++ ASChatTelegramBot/ConsoleRequestTest/Program.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using ConsoleRequestTest.Model;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -11,17 +13,83 @@ namespace ConsoleRequestTest
     {
         public static void Main(string[] args)
         {
-            string token = "H4sIAAAAAAAAAB2OyZKCMABEv8ipABLxiKCYCGHfcrEgoCxRURCErx9nDn3p5VWXM65yg9V2jf1wQQKpUY%2Funsw0BFHbJZGGtz%2FljJdU3E7URzCe8UzjQ4vqqS4k3BVGWCf%2BVNOkmlDz%2BJBm%2F9VVshr3Y2q4KhLvkf%2F3OGfz%2Fx4FgkfcCIeJQHZB62mBcP1nZHev%2BzJq65YuNLBksrDBCnBNfACo4cpm0C5ETAdi7Ndf%2F9tiotWQ6u9LKn46GssgEXn7xyBG%2BrEC9LEDNpCm4PYfI3YnM%2BCc6Opg6UyyAxdQ3WtI43F0Bz%2BR5kJP36NBSLO3DzvJj%2B%2BoviaV71ADHsNx6%2FTKwViy2yqFGNHTW1j3Yns4jv0tYmsdOS%2FOABhcYyW46vKS7VmhTy5Fql5eAM1H7flQUj2LHlny1EjfdYdai%2FB0Ia9xzYu88l97GbLPe50HQIW8bcgmrxPcRDvVN7SVeeKGxMTTORKdnXgC4GKzVR6%2BHfSNuy6WpbOjJnE5wlK5p7tjObGg6YdNqRSUnYdNeEMbmB2KwTPteVb2Qg%2FJHV4MzfSm7UjUpbyk9mdyGg%2Fx9niedaOrGc%2BCG5QLdH4uviZrvRgaEmmpuqOm4Asi1Z1K30KCb0oIkocDHrp0fYo4nAAXE%2Bs6qr%2BCWNTyYwIAAA%3D%3D";
-            var client = new RestClient("https://sandbox.hotmart.com/payments/api/v1/subscriptions?status=CANCELLED_BY_SELLER&status=ACTIVE");
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("authorization", "Bearer " + token + "");
-            request.AddHeader("Content-Type", "application/json");            
+            var authentication = new Authentication();
+            RestClient client = null;
+            IRestResponse response = null;
+            var signatures = new Signatures();
+            var newPurchases = new NewPurchases();
+            string content = null;
+            int value = 1;
+            string client_id = "4f112197-67e9-4ed0-9e40-077944d4c64e";
+            string basic = "NGYxMTIxOTctNjdlOS00ZWQwLTllNDAtMDc3OTQ0ZDRjNjRlOjc3MGNhNmM5LTRmOGUtNDMyMi05NjYyLTQxYmRkYWJmNDE3Mg==";
+            string client_secret = "770ca6c9-4f8e-4322-9662-41bddabf4172";
+            string authURL = "https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id="+
+                client_id+"&client_secret="+client_secret;
+            
 
-            var response = client.Execute(request);
-            var content = response.Content;
-            var signatures = JsonConvert.DeserializeObject<Signatures>(response.Content);
+
+            authentication = Authentication(authURL, basic, client_id, client_secret);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", "Bearer " + authentication.access_token + "");
+            request.AddHeader("Content-Type", "application/json");
+
+            switch (value)
+            {
+                case 1: //obter assinaturas
+                        string assinaturas = "https://sandbox.hotmart.com/payments/api/v1/subscriptions?status=CANCELLED_BY_SELLER&status=ACTIVE";
+                        signatures = GetResponseFromHotmart(client, request, assinaturas, authentication.access_token, new Signatures());
+                        break;
+                case 2: //obter compras assinantes
+                        string subscriber_code = signatures.items[0].subscriber_code;
+                        string comprasAssinantes = "https://sandbox.hotmart.com/payments/api/v1/subscriptions/"+subscriber_code+"/purchases";
+                        newPurchases = GetResponseFromHotmart(client, request, comprasAssinantes, authentication.access_token, new NewPurchases());
+                    break;
+                default:
+                    break;
+            }
+                        
             Console.WriteLine(content);
             Console.ReadLine();
+        }
+
+        public static T GetResponseFromHotmart<T>(RestClient client, RestRequest method , string url, string token, T model)
+        {
+            try
+            {
+                IRestResponse response = null;
+                client = new RestClient(url);
+                response = client.Execute(method);
+                string content = response.Content;
+                model = JsonConvert.DeserializeObject<T>(response.Content);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return model;
+        }
+
+        public static Authentication Authentication(string url, string basic,string client_id, string client_secret)
+        {
+            var authentication = new Authentication();
+            try
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Authorization", "Basic " + basic);
+                request.AddHeader("Content-Type", "application/json");
+
+                var response = client.Execute(request);
+                authentication = JsonConvert.DeserializeObject<Authentication>(response.Content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return authentication;
         }
     }
 }
