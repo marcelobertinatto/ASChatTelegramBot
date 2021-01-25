eaed432626429cf280b8e0d1e5b0089bccc5ce9b
marcelo <marcelobertinatto@hotmail.com>
2021-01-25 13:50:17 +0000

    missing files again

--- ASChatTelegramBot/ConsoleRequestTest/Model/Authentication.cs
+++ ASChatTelegramBot/ConsoleRequestTest/Model/Authentication.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace ConsoleRequestTest.Model
 {
     public class Authentication
     {
-        public Authentication()
-        {
-        }
+        [JsonProperty("access_token")]
+        public string access_token { get; set; }
+
+        [JsonProperty("token_type")]
+        public string token_type { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int expires_in { get; set; }
+
+        [JsonProperty("scope")]
+        public string scope { get; set; }
+
+        [JsonProperty("jti")]
+        public string jti { get; set; }
+        
     }
 }
