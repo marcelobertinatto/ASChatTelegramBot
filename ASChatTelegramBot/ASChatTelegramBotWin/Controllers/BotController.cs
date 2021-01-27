28ea5e00855fe6586baa8554f87b9e6b6ea988fe
Marcelo Bertinatto <marcelobertinatto@hotmail.com>
2021-01-27 14:04:54 +0000

    docker files and hotmart api

---
+++ ASChatTelegramBot/ASChatTelegramBotWin/Controllers/BotController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ASChatTelegramBotWin.Controllers
+{
+    [ApiController]
+    [Route("api/bot")]
+    public class BotController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            TelegramBotClient client = new TelegramBotClient("1417186445:AAGFG-jByzgAEhaZRAKLnnJOigAXbzM8dhU");
+
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
